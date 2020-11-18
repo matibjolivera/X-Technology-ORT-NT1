697c0657cfb0a3c8d5ff31a8871ef1a3ac58e3d5
altman44 <nikoaltman505@gmail.com>
2020-11-18 19:44:04 -0300

    Added products to Configuration/ProductConfiguration.cs

--- Configuration/ProductConfiguration.cs
+++ Configuration/ProductConfiguration.cs
@@ -17,20 +17,42 @@ namespace X_Technology_ORTv2.Configuration
                 new Product
                 {
                     Id = 1,
-                    Title = "Product_Test 1",
-                    Price = 200
+                    Title = "Auriculares",
+                    Price = 200,
+                    Brand = "Samsung",
+                    Sku = "1",
+                    Category = "Sonido",
+                    ImageUrl = "https://www.google.com/url?sa=i&url=https%3A%2F%2Ftiendanaranja.com.py%2Ftienda-movil%2F21699-auriculares-samsung-level-on-wireless-pro-bluetooth.html&psig=AOvVaw1FI-87pR0hRpty8iNwB5Gj&ust=1605825463702000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLCDq5WUje0CFQAAAAAdAAAAABAG"
                 },
                 new Product
                 {
                     Id = 2,
-                    Title = "Product_Test 2",
-                    Price = 160
+                    Title = "Parlante",
+                    Price = 160,
+                    Brand = "JBL",
+                    Sku = "3",
+                    Category = "Sonido",
+                    ImageUrl = "https://d34zlyc2cp9zm7.cloudfront.net/products/7840b4935124524a1351becd203fc8ff992b1785a1ffaf8287a26aadb90953fc.jpg_1000"
                 },
                 new Product
                 {
                     Id = 3,
-                    Title = "Product_Test 3",
-                    Price = 350
+                    Title = "Television",
+                    Price = 350,
+                    Brand = "LG",
+                    Sku = "3",
+                    Category = "Entretenimiento",
+                    ImageUrl = "https://http2.mlstatic.com/D_NQ_NP_2X_930556-MLA41195666242_032020-V.webp"
+                },
+                new Product
+                {
+                    Id = 4,
+                    Title = "Television",
+                    Price = 400,
+                    Brand = "Samsung",
+                    Sku = "1",
+                    Category = "Entretenimiento",
+                    ImageUrl = "https://i1.wp.com/clipset.com/wp-content/uploads/2016/05/samsung-smart-tv.jpg?fit=916%2C600&ssl=1"
                 }
             );
         }
