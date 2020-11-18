14f9a8111bd5c25b7b815b0903a294fc21aeb0fe
altman44 <nikoaltman505@gmail.com>
2020-11-18 19:35:39 -0300

    Adding Configuration/ProductConfiguration.cs

---
+++ Configuration/ProductConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using X_Technology_ORTv2.Models;
+
+namespace X_Technology_ORTv2.Configuration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Products");
+            builder.HasData(
+                new Product
+                {
+                    Id = 1,
+                    Title = "Product_Test 1",
+                    Price = 200
+                },
+                new Product
+                {
+                    Id = 2,
+                    Title = "Product_Test 2",
+                    Price = 160
+                },
+                new Product
+                {
+                    Id = 3,
+                    Title = "Product_Test 3",
+                    Price = 350
+                }
+            );
+        }
+    }
+}
