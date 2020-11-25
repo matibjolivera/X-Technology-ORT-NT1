383d713359746e90f18633dbeb1d7afd9c59536e
Matias Olivera <matibjolivera@gmail.com>
2020-11-25 00:06:49 -0300

    checkout: Billing constructor

--- Models/Billing.cs
+++ Models/Billing.cs
@@ -2,6 +2,18 @@ namespace X_Technology_ORTv2.Models
 {
     public class Billing
     {
+        public Billing()
+        {
+        }
+
+        public Billing(string firstname, string lastname, string document, string email)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+            Document = document;
+            Email = email;
+        }
+
         public int Id { get; set; }
         
         public string Firstname { get; set; }
